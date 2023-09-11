cb7023963f7340b7b7de04966831adcf46c314f4
arnob-das <arnobdas001@gmail.com>
2023-09-11 16:21:26 +0600

    third

---
+++ 217. Contains Duplicate .cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int>s;
+        for(auto i:nums){
+            if(s.find(i)!=s.end())return true;
+            else s.insert(i);
+        }
+        return false;
+    }
+};
---
+++ 997. Find the Town Judge .cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int>judge(n+1,0);
+        for(auto x:trust){
+            judge[x[0]]--;
+            judge[x[1]]++;
+        }
+
+        for(int i=1;i<=n;i++){
+            int value =judge[i];
+            if(value==n-1){
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+};
