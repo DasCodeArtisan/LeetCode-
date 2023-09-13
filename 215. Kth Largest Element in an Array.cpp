5664e8b26e6b64e2874ddca3e57873bec1f46aad
arnob-das <arnobdas001@gmail.com>
2023-09-13 19:47:15 +0600

    5th

---
+++ 215. Kth Largest Element in an Array.cpp
@@ -0,0 +1,84 @@
+// nth_element
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        nth_element(nums.begin(),nums.begin()+k-1,nums.end(),greater<int>());
+        return nums[k-1];
+    }
+};
+
+//partial_sort
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        partial_sort(nums.begin(),nums.begin()+k,nums.end(),greater<int>());
+        return nums[k-1];
+    }
+};
+
+//min heap
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (int num : nums) {
+            pq.push(num);
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+        return pq.top();
+    }
+};
+
+// max heap
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq(nums.begin(), nums.end());
+        for (int i = 0; i < k - 1; i++) {
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
+
+// Quick select Algo
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int left = 0, right = nums.size() - 1, kth;
+        while (true) {
+            int idx = partition(nums, left, right);
+            if (idx == k - 1) {
+                kth = nums[idx];
+                break;
+            }
+            if (idx < k - 1) {
+                left = idx + 1;
+            } else {
+                right = idx - 1;
+            }
+        }
+        return kth;
+    }
+private:
+    int partition(vector<int>& nums, int left, int right) {
+        int pivot = nums[left], l = left + 1, r = right;
+        while (l <= r) {
+            if (nums[l] < pivot && nums[r] > pivot) {
+                swap(nums[l++], nums[r--]);
+            }
+            if (nums[l] >= pivot) {
+                l++;
+            }
+            if (nums[r] <= pivot) {
+                r--;
+            }
+        }
+        swap(nums[left], nums[r]);
+        return r;
+    }
+};
